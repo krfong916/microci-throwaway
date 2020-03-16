60bfe78a526ea83e4b9b2fc83cdc852b2b374961
Kyle Fong <krfong@ucsc.edu>
2020-03-16 14:58:09 -0700

    test heroku and docker registry deploy

--- routes/reports.router.js
+++ routes/reports.router.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { getReportsController } from '../features/reports/useCases/getReports/index';
 const reportsRouter = express.Router();
 
+reportsRouter.get('/', (req, res, next) => {
+  res.send('welcome to microci test!');
+});
+
 reportsRouter.get('/reports/:id', (req, res, next) => {
   getReportsController.execute(req, res, next);
 });
