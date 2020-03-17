481e4e7e434bc49434ee6396dd26fe43d4c2bb09
Kyle Fong <krfong@ucsc.edu>
2020-03-16 17:48:37 -0700

    test docker image heroku deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-# Authenticate to Docker Hub and pull the latest image
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker pull "$DOCKER_USERNAME"/microci-throwaway_server:latest
+docker images
 # Deploy image to Heroku
 echo "${HEROKU_AUTH_TOKEN}" | docker login --username="$DOCKER_USERNAME" --password-stdin registry.heroku.com
 docker tag "$DOCKER_USERNAME"/microci-throwaway_server registry.heroku.com/microci-test/web
-docker images
-echo "test container release"
-heroku container:push web
-heroku container:release web
-        
+docker push registry.heroku.com/microci-test/web
+# docker images
+# echo "test container release"
+# heroku container:login
+# heroku container:push web
+# heroku container:release web
