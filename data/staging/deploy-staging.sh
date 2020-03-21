8f229db053bc86f546e300abb201efcb97a6d3b9
Kyle Fong <krfong@ucsc.edu>
2020-03-20 19:14:04 -0700

    a

--- data/staging/deploy-staging.sh
+++ data/staging/deploy-staging.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo `pwd`
-if [[ $(cat './init.staging.check.sql' | heroku pg:psql) ]]; then
+if [[ $(cat ./data/staging/init.staging.check.sql | heroku pg:psql) ]]; then
 echo "schema and data exists, dropping relations..."
-  [[ $(cat './init.staging.drop.sql' | heroku pg:psql) ]];
+  [[ $(cat ./data/staging/init.staging.drop.sql | heroku pg:psql) ]];
   echo "relations dropped!"
   exit 1
 else
   echo "schema and data do not exist, seeding..."
-  [[ $(cat './init.staging.sql' | heroku pg:psql) ]];
+  [[ $(cat ./data/staging/init.staging.sql | heroku pg:psql) ]];
   echo "seeded!"
   exit 1
 fi
