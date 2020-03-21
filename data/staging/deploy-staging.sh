a93168e5ec257d6d8713c4e349b46f5e313d1f84
Kyle Fong <krfong@ucsc.edu>
2020-03-20 23:28:48 -0700

    ci/cd staging smoke test

--- data/staging/deploy-staging.sh
+++ data/staging/deploy-staging.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
-echo `pwd`
-if [[ $(cat "./data/staging/init.staging.check.sql" | heroku pg:psql) ]]; then
-  echo "the schema and data exist, dropping relations..."
-  [[ $(cat "./data/staging/init.staging.drop.sql" | heroku pg:psql) ]];
-  echo "dropped relations"
-  echo "reseeding"
-  [[ $(cat "./data/staging/init.staging.sql" | heroku pg:psql) ]];
-  echo "seeded"
-  exit 1
-else
-  echo "the schema and data do not exist, seeding..."
-  [[ $(cat "./data/staging/init.staging.sql" | heroku pg:psql) ]];
-  echo "seeded"
-  exit 1
-fi
+echo "dropping relations, resetting the database..."
+heroku pg:reset DATABASE --confirm $HEROKU_APP
+echo "reset the database"
+echo "reseeding"
+cat ".data/staging/init.staging.sql" | heroku pg:psql
